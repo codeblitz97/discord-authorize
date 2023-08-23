e50a711f1b0b0adb6c81a9814b1a8f722e266fe4
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-08-23 19:58:28 +0600

    Add dependencies check

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
+import fs from "fs";
+import path from "path";
+import { exit } from "process";
+const pkg = require("../package.json");
+import { snowflake } from "./global";
+
+const requiredDependencies: snowflake[] = Object.keys(pkg.dependencies);
+
+const missingDependencies: snowflake[] = requiredDependencies.filter((dep) => {
+  try {
+    require.resolve(dep);
+    return false;
+  } catch (err) {
+    return true;
+  }
+});
+
+if (missingDependencies.length > 0) {
+  console.error(
+    `Required dependencies are missing: ${missingDependencies.join(", ")}`
+  );
+  exit(1);
+}
+
+const authFolderPath = path.join(__dirname, "auth");
+
+try {
+  if (!fs.existsSync(authFolderPath)) {
+    throw new Error(
+      "The authentication folder doesn't exist. Re-installing may fix the error."
+    );
+  }
+} catch (error: any) {
+  console.error(error.message);
+  exit(1);
+}
+
 export * from "./auth";
 export * from "./util";
+export * from "./global";
